94f1fab55854daebe2af60d405f44406d66b10b1
Aaron-Reyes-Rodriguez <aaronreyesrodriguez504@gmail.com>
2025-01-18 15:46:01 -0800

    bootstrap added

--- Backend/JS/website.js
+++ Backend/JS/website.js
@@ -1,32 +1,12 @@
-const form = document.getElementById('uploadForm');
-const fileInput = document.getElementById('fileInput');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const file = fileInput.files[0];
-
-  if (file) {
-    const formData = new FormData();
-    formData.append('pdfFile', file);
-
-    // Make an API call to upload the file
-    fetch('/upload', {
-      method: 'POST',
-      body: formData
-    })
-    .then(response => {
-      // Handle the response from the server
-      if (response.ok) {
-        console.log('File uploaded successfully');
-      } else {
-        console.error('File upload failed');
-      }
-    })
-    .catch(error => {
-      console.error('Error uploading file:', error);
-    });
-  } else {
-    console.error('No file selected');
+// JavaScript to preview the image when a file is selected
+document.getElementById('formFile').addEventListener('change', function(event) {
+  var reader = new FileReader();
+  
+  reader.onload = function() {
+    var imagePreview = document.getElementById('imagePreview');
+    imagePreview.src = reader.result;  // Set the image preview source to the file
+    imagePreview.style.display = 'block'; // Display the image preview
   }
+
+  reader.readAsDataURL(this.files[0]);  // Read the selected file as a data URL
 });
