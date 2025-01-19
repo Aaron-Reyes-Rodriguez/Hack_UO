5ff8e7da90b271354e0905d12a3e82909defcc26
Clio Tsao <clioit01@gmail.com>
2025-01-18 23:21:07 -0800

    updated time parsing for 24 hour format

--- Backend/parsehtml.py
+++ Backend/parsehtml.py
@@ -50,20 +50,42 @@ def save_class_info(soup):
     course_total = [' '.join(z) for z in zip(course_nums_list, course_names_list)]
     list_to_dict(course_total, times_list, days_list)
 
+def convert_to_24_hour_start_end(time_range):
+    # Split the time range into start and end times
+    start_time, end_time = time_range.split(" - ")
+
+    def to_24_hour(time):
+        # parse out am/pm
+        period = time[-2:].lower()
+        hours, minutes = map(int, time[:-2].split(":"))
+
+        # to 24-hour
+        if period == "pm" and hours != 12:
+            hours += 12
+        elif period == "am" and hours == 12:
+            hours = 0
+
+        # format back to string
+        return f"{hours:02}:{minutes:02}"
+
+    # convert start and end times
+    start = to_24_hour(start_time)
+    end = to_24_hour(end_time)
+
+    return start, end
+
 def list_to_dict(course_total, times_list, days_list):
     #fix times
     start_times_list = []
     end_times_list = []
     for time in times_list:
-        start_time, end_time = time.split(" - ")
+        start_time, end_time = convert_to_24_hour_start_end(time)
         start_times_list.append(start_time)
         end_times_list.append(end_time)
-    print(start_times_list)
-    print(end_times_list)
-    #for str in times_list:
-    #    tempstart = str.
+    # print(start_times_list)
+    # print(end_times_list)
 
-    keys = ['Course Name', 'Day(s)', 'Time']
+    keys = ['Course Name', 'Day(s)', 'Start Time', 'End Time']
     num_courses = len(course_total)
     course_dict = {}
     schedule = []
@@ -71,10 +93,11 @@ def list_to_dict(course_total, times_list, days_list):
         course_info = []
         course_info.append(course_total[i])
         course_info.append(days_list[i])
-        course_info.append(times_list[i])
+        course_info.append(start_times_list[i])
+        course_info.append(end_times_list[i])
         course_dict = dict(zip(keys, course_info))
         schedule.append(course_dict)
-    # print(times_list)
+    print(schedule)
 
 
 # untested addition
